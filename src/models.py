42cd9e50d2ca8d7b324d38f50f2f004fd6ef7149
Rebillot <125141214+Rebillot@users.noreply.github.com>
2023-05-15 21:24:20 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -3,27 +3,57 @@ import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
-from eralchemy2 import render_er
+from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'users'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(50), nullable= False, unique = True)
+    password = Column(String(50), nullable = False)
+    
 
-class Address(Base):
-    __tablename__ = 'address'
+class Character(Base):
+    __tablename__ = 'character'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(50), nullable=False)
+    homeworld = Column(String(250), nullable = False)
+    age = Column (Integer, nullable = True)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    diameter = Column(Integer, nullable=False)
+    gravity = Column(String, nullable=False)
+    rotation = Column(Integer, nullable=False)
+
+class Starships(Base):
+    __tablename__ = 'vehicles'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    model = Column(String(50), nullable=False)
+    starship_class =Column(String(50), nullable=False)
+    length = Column(Integer, nullable=False)
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    
+
+    
+
+
+    
 
     def to_dict(self):
         return {}
